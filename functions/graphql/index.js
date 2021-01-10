f26818a812944170ea4b1da3d3931264776fa85b
Muhammad Minhal Javed <minhal47@gmail.com>
2021-01-10 21:46:07 +0500

    'update'

--- functions/graphql/index.js
+++ functions/graphql/index.js
@@ -2,16 +2,38 @@ const { ApolloServer, gql } = require("apollo-server-lambda");
 
 const typeDefs = gql`
   type Query {
-    hello: String
+    todos: [Todo]!
+  }
+  type Todo{
+      id:ID!
+      text:String!
+      done:Boolean!
+  }
+  type Mutation{
+      addTodo(text:String!):Todo
+      updateTodoDone(id:ID!):Todo
+
   }
 `;
 
+const todos={};
+let todoIndex=0;
+
 const resolvers = {
   Query: {
-    hello: (root, args, context) => {
-      return "Hello, world!";
+    todos: () => {
+      return Object.values(todos);
     }
+  },
+  Mutation:{
+      addTodo:(_,{text})=>{
+          todoIndex++;
+        const id=`key-${todoIndex}`
+        todos[id]={id,text,done:false}
+      },
+      updateTodoDone:(_,{id})=>{}
   }
+
 };
 
 const server = new ApolloServer({
